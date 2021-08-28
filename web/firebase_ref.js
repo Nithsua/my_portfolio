bbd157eff104604c3f1d2b911d40e99b7af7960b
Nivas Muthu M G <nivasmuthu452@gmail.com>
2021-08-28 11:29:19 +0530

    Include firebase_ref.js

---
+++ web/firebase_ref.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDuNi5gUu3-CQwU_QPlQhsghz6wBTwefL8",
+  authDomain: "nitsua-portfolio.firebaseapp.com",
+  projectId: "nitsua-portfolio",
+  storageBucket: "nitsua-portfolio.appspot.com",
+  messagingSenderId: "78308565812",
+  appId: "1:78308565812:web:67ec00cfa7a3cf94013b61",
+  measurementId: "G-8JH5CDN4E1"
+};
